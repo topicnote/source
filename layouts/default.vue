424f9e65334dbeaf02ad6f657f6237bd739726d2
krgpi <yknoaki@gmail.com>
2019-06-25 22:21:25 +0900

    deploy方法修正

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+<div>
+    <header>
+  <a href="/" class="logo">TopicNote</a>
+  <a href="/" class="button">Project</a>
+  <a href="/about" class="button">Team</a>
+  <!-- <a href="blog" class="button">Blog</a> -->
+   <!-- <a href="demo" class="button">Demo</a> -->
+    <a href="/join" class="button">Join</a>
+</header>
+
+</div>
+  <nuxt/>
+
+<div>
+    <footer>
+  <p>©2018-2019 TopicNote</p>
+</footer>
+</div>
+</div>
+</template>
+
---
+++ pages/blog.vue
@@ -0,0 +1,13 @@
+<template>
+    <body>
+   <h1>About This Blog</h1>
+    <p>このブログでは、チーム内の認識の統一や開発の記録を目的として、内外向けに書かれています。技術的な部分だけにとどまらず、設計思想やその時々に考えていたことを書き留めることで、行き詰まったときに立ち帰れる場所になればと思います。</p>
+      <div class="col-sm-12">
+    <div class="card fluid">
+         <div class="section">AMP HTMLを使ったトピックページの実装</div>
+  <!-- <div class="section"></div> -->
+  <!-- <img src="image.png" class="section media"/> -->
+    </div>
+  </div>
+    </body>
+</template>
