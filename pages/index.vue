54dfcd9d40cc353e15245c876184cf62039d4455
krgpi <yknoaki@gmail.com>
2019-06-25 22:54:12 +0900

    set col

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,37 @@
 <template>
   <body>
+    <div class="row">
+      <div class="col-lg-offset-1 col-lg-5 col-sm-12">
+        <h2>プロジェクト解説スライド</h2>
+        <p><iframe src="//www.slideshare.net/slideshow/embed_code/key/s2QW9dw5YPwNom" width="595" height="485"
+            frameborder="0" marginwidth="0" marginheight="0" scrolling="no"
+            style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen>
+          </iframe>
+          <div style="margin-bottom:5px"> <strong> <a href="//www.slideshare.net/secret/s2QW9dw5YPwNom"
+                title="About TopicNote 20190625" target="_blank">About TopicNote 20190625</a> </strong> </div>
+        </p>
+      </div>
+      <div class="col-lg-4 col-sm-12">
+        <h1>Topic Note とは</h1>
 
-    <h1>Topic Note とは</h1>
-    <p>インターネット上に流通する全情報を追うのは不可能である。</p>
-    <p>インターネットユーザーの多くは、SNSやキュレーションサイトを主な情報取得窓口としている。</p>
-    <p>これらのサービスから情報を得るとき、ユーザーは自ずとコンテンツ自体や発信元にフォーカスさせられる。
-    一方で、物事の流れや繋がりへのフォーカスは薄くなる。</p>
-    <p>そこで、Topic Noteでは、情報内容に着目し、ユーザーにその繋がりを見せることで、より能率的な情報取得ツールを提供できると考え、そのための情報抽出アルゴリズムや効率的な配信方法を研究、同時に開発と実装を行う。</p>
-<br><br>
-<h2>プロジェクト解説スライド</h2>
-<p><iframe src="//www.slideshare.net/slideshow/embed_code/key/s2QW9dw5YPwNom" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="//www.slideshare.net/secret/s2QW9dw5YPwNom" title="About TopicNote 20190625" target="_blank">About TopicNote 20190625</a> </strong> </div>
-</p> </body>
+        <p>インターネット上に流通する全情報を追うのは不可能である。</p>
+        <p>インターネットユーザーの多くは、SNSやキュレーションサイトを主な情報取得窓口としている。</p>
+        <p>これらのサービスから情報を得るとき、ユーザーは自ずとコンテンツ自体や発信元にフォーカスさせられる。
+          一方で、物事の流れや繋がりへのフォーカスは薄くなる。</p>
+        <p>そこで、Topic Noteでは、情報内容に着目し、ユーザーにその繋がりを見せることで、より能率的な情報取得ツールを提供できると考え、そのための情報抽出アルゴリズムや効率的な配信方法を研究、同時に開発と実装を行う。
+        </p>
+      </div>
+    </div>
+    <br><br>
+  </body>
 </template>
 
 <script>
-export default {
-  layout: 'blog',
-  head: {
-    title: 'About Us | TopicNote'
-  },
-  // ページコンポーネントの定義
-}
+  export default {
+    layout: 'blog',
+    head: {
+      title: 'About Us | TopicNote'
+    },
+    // ページコンポーネントの定義
+  }
 </script>
